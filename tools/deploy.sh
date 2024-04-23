d888d73d87dbb154bf6bea3c44412ad11aab94ad
Dan Herbert <microdeploys-dan@hrbrt.co>
2024-04-23 11:55:52 -0400

    Add deploy.sh script

---
+++ tools/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script can update itself while running (since it updates through git)
+# All commands must happen within these curly brace blocks to ensure everything
+# loads into memory before executing.
+{
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+project_root=$(cd -- "$script_dir" && git rev-parse --show-superproject-working-tree)
+# If not running as a submodule, the project root will be empty.
+if [[ -n "$project_root" ]]; then
+    project_root=$(git rev-parse --show-toplevel)
+fi
+
+set -euv
+
+cd "$project_root"
+if [[ $(git status --porcelain | wc -l) -gt 0 ]]; then
+    git stash push --include-untracked \
+        --message "Publishing content $(date -u '+%Y-%m-%dT%H:%M:%SZ')"
+fi
+if [[ $(git branch --show-current) != 'main' ]]; then
+    git checkout --force main
+fi
+git pull --force
+npm install
+npm run publish
+}; exit
