a75b028516f6fa29d85e1516fb6030bd23ef60a1
Dan Herbert <microdeploys-dan@hrbrt.co>
2024-04-23 10:33:32 -0400

    Add example site to project

---
+++ example/ts/app.ts
@@ -0,0 +1,7 @@
+//  SPDX-FileCopyrightText: 2024 Daniel P. Herbert <microdeploys-dan@hrbrt.co>
+//  SPDX-License-Identifier: MIT
+//  @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
+
+document.body.classList.add('js');
+
+//  @license-end
